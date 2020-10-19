a6fcf6845129e8e803412ba5b89e6034f8b913b4
tatchi <tinencor@gmail.com>
2020-10-19 22:01:36 +0200

    shasum

--- .ci-self/checksum.js
+++ .ci-self/checksum.js
@@ -1,11 +1,8 @@
 let crypto = require('crypto'),
-  path = require('path'),
   fs = require('fs');
 
 let algorithm = 'sha1',
   shasum = crypto.createHash(algorithm);
-
-// let cwd = process.cwd();
 
 s = fs.ReadStream(process.argv[2]);
 
